5ba9be283565c154ee98821f12f6869054ec1e77
Admir Memic <me.admir@gmail.com>
2014-06-16 17:52:42 +0200

    helper.py  - get_q_indexes now has dx_bins as argument
    qTable.py
     - changed the decay rate of alpha to K/(K + t)
     - input signal u changed

--- helper.py
+++ helper.py
@@ -1,7 +1,32 @@
 import numpy
 
 
-def get_q_indexes(policy, x, dx, x_bins):
+# def get_q_indexes(policy, x, dx, x_bins):
+#     if policy > 0:
+#         policy_index = 1
+#     else:
+#         policy_index = 0
+#
+#     if x <= x_bins[0]:
+#         x_index = 0
+#     elif x >= x_bins[-1]:
+#         x_index = len(x_bins) - 1
+#     else:
+#         x_index = numpy.digitize([x], x_bins)[0] - 1
+#
+#     knee_point = 0.75
+#
+#     if dx <= -knee_point:
+#         dx_index = 0
+#     elif abs(dx) < knee_point:
+#         dx_index = 1
+#     elif dx >= knee_point:
+#         dx_index = 2
+#
+#     return [policy_index, x_index, dx_index]
+
+
+def get_q_indexes(policy, x, dx, x_bins, dx_bins):
     if policy > 0:
         policy_index = 1
     else:
@@ -14,13 +39,11 @@ def get_q_indexes(policy, x, dx, x_bins):
     else:
         x_index = numpy.digitize([x], x_bins)[0] - 1
 
-    knee_point = 0.75
-
-    if dx <= -knee_point:
+    if dx <= dx_bins[0]:
         dx_index = 0
-    elif abs(dx) < knee_point:
-        dx_index = 1
-    elif dx >= knee_point:
-        dx_index = 2
+    elif dx >= dx_bins[-1]:
+        dx_index = len(dx_bins) - 1
+    else:
+        dx_index = numpy.digitize([dx], dx_bins)[0] - 1
 
     return [policy_index, x_index, dx_index]
--- qTable.py
+++ qTable.py
@@ -19,30 +19,48 @@ time_step = 1 / float(500)
 
 u = []
 
-# SISO System
-for k0 in range(667):
+# Generating input signal
+
+for k00 in range(50):
+    u.append(k00 * (0.5 / 50))
+
+for k0 in range(617):
     u.append(0.5)
 
-for k1 in range(666):
+for k01 in range(50):
+    u.append((-0.5 / 50) * k01 + 0.5)
+
+for k1 in range(616):
     u.append(0)
 
-for k2 in range(667):
+for k12 in range(50):
+    u.append((-0.5 / 50) * k12)
+
+for k2 in range(567):
     u.append(-0.5)
 
-e_bins = np.linspace(-2, 2, 100)
+for k22 in range(50):
+    u.append((0.5 / 50) * k22 - 0.5)
 
-Q = np.zeros((2, 100, 3))
+# u = []
+# for kaka in range(2000):
+# u.append(math.sin(kaka * time_step * math.pi / 2))
+
+e_bins = np.linspace(-2, 2, 100)
+de_bins = np.linspace(-4, 4, 5)
+
+Q = np.zeros((2, len(e_bins), len(de_bins)))
 # noinspection PyCallingNonCallable
 x_k = np.matrix([[0],
                  [0]])
 y_k = (C * x_k)[0, 0]
 set_point_k = u[0]
 e_k = set_point_k - y_k
-de_k = (e_k - 0) / time_step
-for k in range(1000000):
-    rangelo = (k + 100000) / 100000
-    alpha = 1 / float(2 ** (rangelo - 1))
-    if k % 100000 == 0:
+de_k = abs(random.gauss(0, 0.02))
+for k in range(500000):
+    rangelo = (k + 5000) / 5000
+    alpha = 20 / float(20 + rangelo)
+    if k % 5000 == 0:
         # noinspection PyCallingNonCallable
         x_k = np.matrix([[random.gauss(0, 0.02)],
                          [random.gauss(0, 0.02)]])
@@ -56,14 +74,14 @@ for k in range(1000000):
     if temp == 0:
         policy = sample([-1, 1], 1)[0]
     else:
-        indexes_0 = helper.get_q_indexes(-1, e_k, de_k, e_bins)
-        indexes_1 = helper.get_q_indexes(1, e_k, de_k, e_bins)
+        indexes_0 = helper.get_q_indexes(-1, e_k, de_k, e_bins, de_bins)
+        indexes_1 = helper.get_q_indexes(1, e_k, de_k, e_bins, de_bins)
         if Q[indexes_0[0], indexes_0[1], indexes_0[2]] >= Q[indexes_1[0], indexes_1[1], indexes_1[2]]:
             policy = -1
         else:
             policy = 1
 
-    indexes_k = helper.get_q_indexes(policy, e_k, de_k, e_bins)
+    indexes_k = helper.get_q_indexes(policy, e_k, de_k, e_bins, de_bins)
 
     Qk = Q[indexes_k[0], indexes_k[1], indexes_k[2]]
 
@@ -73,9 +91,11 @@ for k in range(1000000):
     set_point_kk = u[(k + 1) % 2000]
     e_kk = set_point_kk - y_kk
     de_kk = (e_kk - e_k) / time_step
+    # if abs(de_kk) > 10:
+    #     de_kk = de_k
 
-    indexes_kk_0 = helper.get_q_indexes(-1, e_kk, de_kk, e_bins)
-    indexes_kk_1 = helper.get_q_indexes(1, e_kk, de_kk, e_bins)
+    indexes_kk_0 = helper.get_q_indexes(-1, e_kk, de_kk, e_bins, de_bins)
+    indexes_kk_1 = helper.get_q_indexes(1, e_kk, de_kk, e_bins, de_bins)
 
     Qmax = max([Q[indexes_kk_0[0], indexes_kk_0[1], indexes_kk_0[2]],
                 Q[indexes_kk_1[0], indexes_kk_1[1], indexes_kk_1[2]]])
@@ -98,17 +118,18 @@ x = [np.matrix([[0],
 y = [(C * x[0])[0, 0]]
 set_point = [u[0]]
 e = [set_point[0] - y[0]]
-de = [(e[0] - 0) / time_step]
+de = [0]
 cs = []
 
 # u = []
 # for kaka in range(2000):
 # u.append(math.sin(kaka * time_step * math.pi / 2))
+
 for k in range(6000):
     t.append(k * time_step)
 
-    indexes_0 = helper.get_q_indexes(-1, e[k], de[k], e_bins)
-    indexes_1 = helper.get_q_indexes(1, e[k], de[k], e_bins)
+    indexes_0 = helper.get_q_indexes(-1, e[k], de[k], e_bins, de_bins)
+    indexes_1 = helper.get_q_indexes(1, e[k], de[k], e_bins, de_bins)
     if Q[indexes_0[0], indexes_0[1], indexes_0[2]] > Q[indexes_1[0], indexes_1[1], indexes_1[2]]:
         policy = -1
     else:
@@ -119,7 +140,10 @@ for k in range(6000):
     cs.append(policy)
     set_point.append(u[(k + 1) % 2000])
     e.append(set_point[k + 1] - y[k + 1])
-    de.append((e[k + 1] - e[k]) / time_step)
+    de_kk = (e[k + 1] - e[k]) / time_step
+    # if abs(de_kk) > 10:
+    #     de_kk = de[k]
+    de.append(de_kk)
 
 del y[-1]
 del set_point[-1]
