ec231b187d2ee4636fd5ea4e8e4efa3f64cdac45
Admir Memic <me.admir@gmail.com>
2014-06-17 13:48:27 +0200

    - changed time_step - tweaked decay of learning rate - changed the immediate reward rule

--- qTable.py
+++ qTable.py
@@ -15,32 +15,41 @@ B = np.matrix([[1.98992813e-03],
 # noinspection PyCallingNonCallable
 C = np.matrix([[0., 39.4784176]])
 
-time_step = 1 / float(500)
+time_step = 1 / float(1200)
 
 u = []
 
 # Generating input signal
 
-for k00 in range(50):
-    u.append(k00 * (0.5 / 50))
+# for k00 in range(50):
+#     u.append(k00 * (0.5 / 50))
+#
+# for k0 in range(617):
+#     u.append(0.5)
+#
+# for k01 in range(50):
+#     u.append((-0.5 / 50) * k01 + 0.5)
+#
+# for k1 in range(616):
+#     u.append(0)
+#
+# for k12 in range(50):
+#     u.append((-0.5 / 50) * k12)
+#
+# for k2 in range(567):
+#     u.append(-0.5)
+#
+# for k22 in range(50):
+#     u.append((0.5 / 50) * k22 - 0.5)
 
-for k0 in range(617):
+for k0 in range(667):
     u.append(0.5)
 
-for k01 in range(50):
-    u.append((-0.5 / 50) * k01 + 0.5)
-
-for k1 in range(616):
+for k1 in range(666):
     u.append(0)
 
-for k12 in range(50):
-    u.append((-0.5 / 50) * k12)
-
-for k2 in range(567):
+for k2 in range(667):
     u.append(-0.5)
-
-for k22 in range(50):
-    u.append((0.5 / 50) * k22 - 0.5)
 
 # u = []
 # for kaka in range(2000):
@@ -57,13 +66,13 @@ y_k = (C * x_k)[0, 0]
 set_point_k = u[0]
 e_k = set_point_k - y_k
 de_k = abs(random.gauss(0, 0.02))
-for k in range(500000):
-    rangelo = (k + 5000) / 5000
-    alpha = 20 / float(20 + rangelo)
-    if k % 5000 == 0:
+for k in range(1000000):
+    rangelo = k / 10000
+    alpha = 100000 / float(100000 + k)
+    if k % 10000 == 0:
         # noinspection PyCallingNonCallable
-        x_k = np.matrix([[random.gauss(0, 0.02)],
-                         [random.gauss(0, 0.02)]])
+        x_k = x_k + np.matrix([[random.gauss(0, 0.01)],
+                               [random.gauss(0, 0.01)]])
         y_k = (C * x_k)[0, 0]
         set_point_k = u[0]
         e_k = set_point_k - y_k
@@ -92,7 +101,7 @@ for k in range(500000):
     e_kk = set_point_kk - y_kk
     de_kk = (e_kk - e_k) / time_step
     # if abs(de_kk) > 10:
-    #     de_kk = de_k
+    # de_kk = de_k
 
     indexes_kk_0 = helper.get_q_indexes(-1, e_kk, de_kk, e_bins, de_bins)
     indexes_kk_1 = helper.get_q_indexes(1, e_kk, de_kk, e_bins, de_bins)
@@ -100,9 +109,14 @@ for k in range(500000):
     Qmax = max([Q[indexes_kk_0[0], indexes_kk_0[1], indexes_kk_0[2]],
                 Q[indexes_kk_1[0], indexes_kk_1[1], indexes_kk_1[2]]])
 
-    r0 = -abs(e_kk)
+    # if abs(e_kk) < 0.005 and abs(de_kk) < 0.05:
+    #     r = 5
+    if abs(e_kk) < abs(e_k):
+        r = 1
+    else:
+        r = 0
 
-    Q[indexes_k[0], indexes_k[1], indexes_k[2]] = (1 - alpha) * Qk + alpha * (r0 + 0.8 * Qmax)
+    Q[indexes_k[0], indexes_k[1], indexes_k[2]] = (1 - alpha) * Qk + alpha * (r - abs(e_kk) + 0.8 * Qmax)
 
     x_k = x_kk
     y_k = y_kk
@@ -142,7 +156,7 @@ for k in range(6000):
     e.append(set_point[k + 1] - y[k + 1])
     de_kk = (e[k + 1] - e[k]) / time_step
     # if abs(de_kk) > 10:
-    #     de_kk = de[k]
+    # de_kk = de[k]
     de.append(de_kk)
 
 del y[-1]
@@ -152,7 +166,7 @@ del de[-1]
 
 f, axarr = plt.subplots(2, sharex=True)
 axarr[0].plot(t, set_point, t, y)
-axarr[1].plot(t, de)
+axarr[1].plot(t, e)
 
 for axis in axarr:
     axis.grid()
