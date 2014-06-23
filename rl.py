2a0bb06b6f2f7cfa7a540db2f3775719794f3033
Admir Memic <me.admir@gmail.com>
2014-06-23 15:49:08 +0200

    - changes due to helper.py change of get_q_indexes function

--- rl.py
+++ rl.py
@@ -5,64 +5,71 @@ import math
 
 
 class Agent:
-    def __init__(self, **kwargs):
+    def __init__(self, manual_exploration=False, **kwargs):
         self.iteration = 0
         self.e_k = 0
         self.e_kk = 0
         self.de_k = 0
         self.de_kk = 0
         self.alpha = 0
+        self.manual_exploration = manual_exploration
+        self.exploration_rate = 1
         # TODO: treba ovo fino napraviti
+        self.actions = kwargs['actions']
         self.e_bins = kwargs['e_bins']
         self.de_bins = kwargs['de_bins']
         self.plant = kwargs['plant']
         self.time_step = kwargs['time_step']
-        self.q = np.zeros((2, len(self.e_bins), len(self.de_bins), 2))
+        self.q = np.zeros((len(self.actions), len(self.e_bins), len(self.de_bins), 2))
 
-    def update_q_table(self, set_point_s):
-        rangelo = self.iteration / 1000
-        indexes_k = helper.get_q_indexes(self.e_k, self.de_k, self.e_bins, self.de_bins)
-
-        temp = random.sample(range(rangelo + 1), 1)[0]
-        if temp == 0:
-            policy_s = random.sample([-1, 1], 1)[0]
+    def update_q_table(self, set_point):
+        state_indexes_k = helper.get_q_indexes(values=[self.e_k, self.de_k], bins=[self.e_bins, self.de_bins])
+        if not self.manual_exploration:
+            self.exploration_rate = 1000 / float(1000 + self.iteration)
+        if self.iteration % 100 == 0:
+            print(self.exploration_rate),
+        choice = helper.weighted_choice([self.exploration_rate, 1 - self.exploration_rate])
+        if choice == 0:
+            policy = random.sample(self.actions, 1)[0]
         else:
-            if self.q[0, indexes_k[0], indexes_k[1], 0] >= self.q[1, indexes_k[0], indexes_k[1], 0]:
-                policy_s = -1
-            else:
-                policy_s = 1
+            argmax_a = np.argmax([self.q[:, state_indexes_k[0], state_indexes_k[1], 0]])
+            policy = self.actions[argmax_a]
 
-        q_k_value = self.q[(policy_s + 1) / 2, indexes_k[0], indexes_k[1], 0]
-        q_k_visited = self.q[(policy_s + 1) / 2, indexes_k[0], indexes_k[1], 1]
+        action_index = helper.get_q_indexes(values=[policy], bins=[self.actions])[0]
+        q_k_value = self.q[action_index, state_indexes_k[0], state_indexes_k[1], 0]
+        q_k_visited = self.q[action_index, state_indexes_k[0], state_indexes_k[1], 1]
 
-        output = self.plant.get_output(policy_s)[0, 0]
+        output = self.plant.get_output(policy)[0, 0]
 
-        self.e_kk = set_point_s - output
+        self.e_kk = set_point - output
         self.de_kk = (self.e_kk - self.e_k) / self.time_step
 
-        indexes_kk = helper.get_q_indexes(self.e_kk, self.de_kk, self.e_bins, self.de_bins)
+        state_indexes_kk = helper.get_q_indexes(values=[self.e_kk, self.de_kk], bins=[self.e_bins, self.de_bins])
 
-        q_max = max([self.q[0, indexes_kk[0], indexes_kk[1], 0], self.q[1, indexes_kk[0], indexes_kk[1], 0]])
+        q_max = max(self.q[:, state_indexes_kk[0], state_indexes_kk[1], 0])
+
+        abs_e_k = math.fabs(self.e_k)
         abs_e_kk = math.fabs(self.e_kk)
-        if abs_e_kk < math.fabs(self.e_k):
-            r = 3
+        if abs_e_kk < abs_e_k:
+            r = 0.5
         else:
             r = 0
 
         self.alpha = 50 / (50 + q_k_visited)
-        self.q[(policy_s + 1) / 2, indexes_k[0], indexes_k[1], 0] = (1 - self.alpha) * q_k_value + self.alpha * (
+        if self.iteration % 100 == 0:
+            print(" " + str(self.alpha))
+
+        self.q[action_index, state_indexes_k[0], state_indexes_k[1], 0] = (1 - self.alpha) * q_k_value + self.alpha * (
             r - abs_e_kk + 0.8 * q_max)
 
-        self.q[(policy_s + 1) / 2, indexes_k[0], indexes_k[1], 1] = q_k_visited + 1
+        self.q[action_index, state_indexes_k[0], state_indexes_k[1], 1] = q_k_visited + 1
 
         self.e_k = self.e_kk
         self.de_k = self.de_kk
         self.iteration += 1
 
     def get_policy(self, state_vector):
-        indexes = helper.get_q_indexes(state_vector[0], state_vector[1], self.e_bins, self.de_bins)
-        if self.q[0, indexes[0], indexes[1], 0] >= self.q[1, indexes[0], indexes[1], 0]:
-            policy = -1
-        else:
-            policy = 1
+        state_indexes = helper.get_q_indexes(values=state_vector, bins=[self.e_bins, self.de_bins])
+        argmax_a = np.argmax([self.q[:, state_indexes[0], state_indexes[1], 0]])
+        policy = self.actions[argmax_a]
         return policy
